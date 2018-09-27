9c49c90121c15355f344335607065cc18a798fd1
2imad <imadyoussoufi@gmail.com>
2018-09-27 12:49:16 +0200

    compile sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,4 @@
 module.exports = grunt => {
-   
-
    // Config
    grunt.initConfig({
      concat: {
@@ -9,16 +7,24 @@ module.exports = grunt => {
              dest:'build/scripts.js'
          },
          css: {
-             src: ['css/*.css'],
+             src: ['css/reset.css' , 'css/bootstrap.css', 'css/styles.css'],
              dest:'build/styles.css'
          } 
+     },
+     sass: {
+         build: {
+             files: [{
+                 src: 'css/sass/styles.scss',
+                 dest: 'xcc/styles.css'
+             }]
+         }
      }  
    });
    
    // load 
-   
    grunt.loadNpmTasks('grunt-contrib-concat');
-
+   grunt.loadNpmTasks('grunt-sass');
+   
    // register task 
    grunt.registerTask('concat-css' , ['concat:css'])
    grunt.registerTask('concat-js' , ['concat:js'])
